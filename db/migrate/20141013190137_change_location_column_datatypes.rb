4fba435f5706f14ea4799961bd7b85b845ad67bd
prth <spratham55@gmail.com>
2015-04-20 13:39:19 +0530

    corrected db migrate file

--- db/migrate/20141013190137_change_location_column_datatypes.rb
+++ db/migrate/20141013190137_change_location_column_datatypes.rb
@@ -1,8 +1,8 @@
 class ChangeLocationColumnDatatypes < ActiveRecord::Migration
   def change
-  	change_column :doctors, :locality, 'integer USING CAST(column_name AS integer)'
-  	change_column :doctors, :city, 'integer USING CAST(column_name AS integer)'
-  	change_column :doctors, :state, 'integer USING CAST(column_name AS integer)'
-  	change_column :doctors, :country, 'integer USING CAST(column_name AS integer)'
+  	change_column :doctors, :locality, 'integer USING CAST(locality AS integer)'
+  	change_column :doctors, :city, 'integer USING CAST(city AS integer)'
+  	change_column :doctors, :state, 'integer USING CAST(state AS integer)'
+  	change_column :doctors, :country, 'integer USING CAST(country AS integer)'
   end
 end
